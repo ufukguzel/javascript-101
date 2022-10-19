29c3d6c09fbf950befb915783e174f3b77f80026
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2022-10-19 16:02:38 +0300

    Days
    Days

---
+++ Gun-Yazdirma.js
@@ -0,0 +1,27 @@
+
+let tarih = new Date();
+let gun = tarih.getDay();
+switch(gun){
+    case 0:
+        console.log("Pazar");
+    break;
+    case 1:
+        console.log("Pazartesi");
+    break;
+    case 2:
+        console.log("Salı");
+    break;
+    case 3:
+        console.log("Çarşamba");
+    break;
+    case 4:
+        console.log("Perşembe");
+    break;
+    case 5:
+        console.log("Cuma");
+    break;
+    case 6:
+        console.log("Cumartesi");
+    break;
+    
+}
