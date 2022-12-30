abc00b48b5859d60f307fa53fc79c99ea30d3260
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2022-12-31 00:54:00 +0300

    boo-who.js
    this function will be find your value

---
+++ boo-who.js
@@ -0,0 +1,7 @@
+function booWho(bool){
+    return typeof bool === "boolean";
+}
+booWho(null);
+
+//bu fonksiyon false değeri döndürür çünkü girilen değer nulldur
+//eğer "boolean" değeri gibi bir değer girilirse mesela "ufuk" true döndürüf
