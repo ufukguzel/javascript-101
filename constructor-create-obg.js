7df628200d9efdf5f8517a4b06126aff11498ecb
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-01-14 16:56:52 +0300

    Constructor-Crreate
    this function allows you to assign the object to a different variable

---
+++ constructor-create-obg.js
@@ -0,0 +1,15 @@
+function Dog(){
+    this.name = "Bady";
+    this.color = "Brown";
+    this.age = 5;
+    this.numLegs = 4;
+}
+
+let roco = new Dog();
+
+roco.color;
+
+roco.name = "Roco";
+roco.name;
+
+//oluşturmuş olduğum Dog a bady tanımladım sonra dou let değişkeni ile oluşturmuş olduğum roco değişkenine bağladım artık roco ile çağırılabilecek.
