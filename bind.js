0bc9bc37e40612c612aa16a0ee3247d459517991
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-05-25 18:53:03 +0300

    Bind

---
+++ bind.js
@@ -0,0 +1,26 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        text: "Hello"
+      };
+//ı define this.handleClick
+      this.handleClick = this.handleClick.bind(this);
+
+    }
+    handleClick() {
+      this.setState({
+        text: "You clicked!"
+      });
+    }
+    render() {
+      return (
+        <div>
+
+          <button onClick = {this.handleClick}>Click Me</button>
+
+          <h1>{this.state.text}</h1>
+        </div>
+      );
+    }
+  };
