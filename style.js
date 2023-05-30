837c1c09fa77e89efbbaf132429a376b977d6f72
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-05-30 23:38:38 +0300

    style.js

---
+++ style.js
@@ -0,0 +1,8 @@
+class Colorful extends React.Component {
+    render() {
+      return (
+        <div style={{color: "red", fontSize: 72}}>Big Red</div>
+      );
+    }
+  };
+  // ı added a style attribute to the div in the code editor to give the text a color of red and font size of 72px.
