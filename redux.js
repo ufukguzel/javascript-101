39232e7bc0a795e76d51fca3041dd19b312ee883
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-06-03 19:10:29 +0300

    Logın

--- redux.js
+++ redux.js
@@ -4,3 +4,29 @@ const reducer = (state = 5) => {
   const store = Redux.createStore(reducer);
 
   //ı started learned redux
+
+//----------------------------------
+
+//const store = Redux.createStore(
+//    (state = 5) => state
+//  );
+  
+//  const currentState = store.getState();
+
+
+//------------------------------------
+let action = {
+    type:'LOGIN'
+  };
+
+
+{/*  
+  const action = {
+    type: 'LOGIN'
+  }
+  
+  function actionCreator(){
+  return action;
+  }
+  */}
+  
