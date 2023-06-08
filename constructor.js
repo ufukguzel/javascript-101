7b90e69c176580198618e3132deceb4cc4a2eae8
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-06-08 20:40:35 +0300

    counstructor
    React and Redux

---
+++ constructor.js
@@ -0,0 +1,14 @@
+class DisplayMessages extends React.Component {
+
+    constructor(props){
+      super(props);
+      this.state={
+        input :'',
+        message :[]
+        
+      }
+    }
+    render() {
+      return <div />
+    }
+  };
